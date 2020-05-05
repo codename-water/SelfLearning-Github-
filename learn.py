54697dff0bf6e4542c6307326d84a90ca99e2984
Paritosh Saini <toparitoshsaini@gmail.com>
2020-05-05 09:51:47 +0530

    Changes done to two files - 1. learn.py, 2. sample.py

--- learn.py
+++ learn.py
@@ -1,1 +1,3 @@
 print('This is a file created for learning purpose, nothing important here, please ignore this gibberish.')
+
+print('This is being added as a change to notice int the git.')
--- sample.py
+++ sample.py
@@ -1,1 +1,3 @@
 print('This is also a sample file for learning purpose.')
+
+print('Change is also done here in this file as the learning method. Still learning the git...')
