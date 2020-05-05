90cc44ca6fbe0cdd9829bb79a3b9eee8f5348133
Paritosh Saini <toparitoshsaini@gmail.com>
2020-05-05 09:38:58 +0530

    My first commit

---
+++ init_function.py
@@ -0,0 +1,12 @@
+class saini:
+    
+    def _init_(self):
+        print('Hi there!, my name is Paritosh Saini.')
+        
+    def add(self):
+        a=int(input('Enter first number:'))
+        b=int(input('Enter second number:'))
+        print('Sum of two numbers is',a+b)
+        
+obj=saini()
+obj.add
