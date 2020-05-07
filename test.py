c29e6341b9e4f119738ee26e6e41066882c58b4d
Paritosh Saini <toparitoshsaini@gmail.com>
2020-05-07 10:45:14 +0530

    Added new file to the directory

---
+++ test.py
@@ -0,0 +1,10 @@
+class test:
+    def __init__(self):
+        print('This is the init function.')
+    
+    def sample(self,a,b):
+        print('sum of two numbers is:-',a+b)
+        
+    
+obj=test()
+obj.sample(4,4)
